8a533c35eed79dee23201422c99d4a8bcd269ec4
Holler János <hollerjanika@gmail.com>
2023-04-06 19:58:23 +0200

    Printing a Sudoku on the page
    Adding a few line to style the Sudoku.

--- includes/header.php
+++ includes/header.php
@@ -2,5 +2,27 @@
 <html>
     <head>
         <title>Sudoku</title>
+        <style>
+            html, body
+            {
+                margin: 0;
+            }
+            table
+            {
+                margin: 50px auto;
+                border: 5px solid #000000;
+                border-collapse: collapse;
+            }
+            table tr td
+            {
+                background-color: #FFFFFF;
+                border: 1px solid #000000;
+                padding: 25px;
+            }
+            table tr td:hover 
+            {
+                background-color: #BFBFBF;
+            }
+        </style>
     </head>
     <body>
--- index.php
+++ index.php
@@ -10,9 +10,18 @@ require_once("includes/header.php");
 // Content
 //==============================
 
-?>
-        <h1>Hello, Blanka!</h1>
-<?php
+echo "\t\t<table>\n";
+
+for ($row = 0; $row < 9; $row++)
+{
+    echo "\t\t\t<tr id=\"$row\">\n";
+    for ($column = 0; $column < 9; $column++)
+    {
+        echo "\t\t\t\t<td id=\"$row$column\"></td>\n";
+    }
+    echo "\t\t\t</tr>\n";
+}
+echo "\t\t</table>\n";
 
 //==============================
 // Including footer
